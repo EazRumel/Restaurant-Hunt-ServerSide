30b5561a6611a766b907bc9acfd2a2d802168720
Eaz Rumel <eazrumel@Eazs-MacBook-Air.local>
2025-07-15 22:24:20 +0600

    updated the api of menu using patch and get for individual item

--- index.js
+++ index.js
@@ -80,6 +80,35 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     })
+app.get("/menu/:id",async(req,res)=>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await menuCollection.findOne(query)
+  res.send(result)
+})
+
+app.patch("/menu/:id",async(req,res)=>{
+  const item = req.body;
+  const id = req.params.id;
+  const filter = {_id:new ObjectId(id)}
+  const updatedDoc = {
+    $set:{
+      name:item.name,
+      price:item.price,
+      category:item.category,
+      recipe:item.recipe,
+      image:item.image
+    }
+  }
+  const result = await menuCollection.updateOne(filter,updatedDoc)
+  res.send(result)
+})
+    app.delete("/menu/:id",verifyToken,verifyAdmin,async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await menuCollection.deleteOne(query)
+      res.send(result)
+    })
     
     //review related apis
     app.get("/reviews", async (req, res) => {
@@ -147,7 +176,7 @@ async function run() {
           role: "admin"
         }
       }
-      const result = await userCollection.updateOne(query, updatedDoc)
+      const result = await userCollection.updateOne(query,updatedDoc)
       res.send(result)
     })
 
